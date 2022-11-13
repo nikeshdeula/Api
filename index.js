1ddac8fabc44660e908183c571a08a1f4d2afb88
nikeshDeula <nikeshdeula.21@gmail.com>
2022-11-13 20:08:13 +0545

    node api

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+const apiData = require("./sdata.json");
+
+app.get("/"), (res, req) => {
+    res.send("Successfully Connected")};
+
+    app.get("/service"), (res, req) => {
+        res.send(apiData);
+    }
+
+    app.listen(port, () => {
+        console.log("Still Connected");
+    });
+
